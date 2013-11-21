ece077ce8d14e15f058ae35bf5683e2a1e709a22
fernferret <fernferret@gmail.com>
2013-11-21 20:52:56 +0430

    Add .proto files and script to compile them

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ SteamKit-Java, is a java port of a library written for interfacing directly with
 The major connection functionality exists, but there are plenty of functions I haven't tested yet.
 
 You must include bouncycastle as a dependency to allow encryption of the packets.
+
+Make sure you install the [JCE](http://www.oracle.com/technetwork/java/javase/downloads/jce-7-download-432124.html).
+
+You can test it with:
+
+    System.out.println(Cipher.getMaxAllowedKeyLength("AES"));
+
+If that prints 128, you don't have JCE installed properly and a test will fail.
---
+++ src/main/proto/compile_proto.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd steamclient
+rm -r ../generated
+mkdir ../generated
+PROTOFILES=( "content_manifest" "encrypted_app_ticket" "iclient_objects" "steammessages_base" "steammessages_clientserver" )
+for protofile in "${PROTOFILES[@]}"
+do
+    echo "Compiling ${protofile}.proto..."
+    protoc --java_out=../generated ${protofile}.proto
+done
+cd ..
+cd generated
+JAVAFILES=( "ContentManifest" "EncryptedAppticket" "IclientObjects" "SteammessagesBase" "SteammessagesClientserver" )
+for javafile in "${JAVAFILES[@]}"
+do
+    echo "Setting package info for ${javafile}"
+    awk 'NR==3{$0="\npackage uk.co.thomasc.steamkit.base.generated;\n"$0}1' ${javafile}.java > ${javafile}.new
+    mv ${javafile}.new ${javafile}.java
+done
+echo "Moving into place..."
+cp *.java ../../java/uk/co/thomasc/steamkit/base/generated/
+cd ..
